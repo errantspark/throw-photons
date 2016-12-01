ca91b52797c59956796e0487d101de23c240d77d
Matt Vana <errantspark@gmail.com>
2016-12-01 04:09:50 -0800

    old code this is the version that's been running our lights for almost 3 years now

---
+++ hue.js
@@ -0,0 +1,160 @@
+var req = new XMLHttpRequest();
+var developerid = 'newdeveloper';
+var basestation = "http://192.168.1.16/";
+var group0uri = basestation + "api/" + developerid + "/groups/0";
+var defcon = {
+    on: true,
+    ct: 280,
+    sat: 255,
+    bri: 255
+};
+
+//gamut of various things in the CIE 1931 color space
+var hueGamut = [
+    [0.675, 0.322],
+    [0.409, 0.518],
+    [0.167, 0.04]
+]
+var delGamut = [
+    [0.675, 0.329],
+    [0.316, 0.598],
+    [0.157, 0.063]
+]
+
+var Group = function(resource) {
+    this.resource = resource;
+    var xml = new XMLHttpRequest();
+    this.xml = xml;
+    var put = function(json) {
+        xml.open('put', resource + "/action", true);
+        xml.send(JSON.stringify(json));
+    };
+    var get = function() {
+        //this probably shouldn't be synchronous maybe? but how fix?
+        xml.open('get', resource, false);
+        xml.send();
+        return JSON.parse(xml.response);
+    };
+    this.put = put;
+    this.get = get;
+};
+
+var Lumi = function(resource) {
+    this.resource = resource;
+    var xml = new XMLHttpRequest();
+    this.xml = xml;
+    var put = function(json) {
+        xml.open('put', resource + "/state", true);
+        xml.send(JSON.stringify(json));
+    };
+    var get = function() {
+        //this probably shouldn't be synchronous maybe? but how fix?
+        xml.open('get', resource, false);
+        xml.send();
+        return JSON.parse(xml.response);
+    };
+    this.get = get;
+    this.put = put;
+};
+
+var toCart = function(r, g, b, d, e, f) {
+    var sum = [r, g, b].reduce(function(a, b) {
+        return a + b
+    })
+    r = (r / sum);
+    g = (g / sum);
+    b = (b / sum);
+    var x = r * d[0] + g * e[0] + b * f[0];
+    var y = r * d[1] + g * e[1] + b * f[1];
+    return [x, y];
+}
+
+var toBary = function(p, r, g, b) {
+    var end = {};
+    end.r = ((g[1] - b[1]) * (p[0] - b[0]) + (b[0] - g[0]) * (p[1] - b[1])) / ((g[1] - b[1]) * (r[0] - b[0]) + (b[0] - g[0]) * (r[1] - b[1]));
+    end.g = ((b[1] - r[1]) * (p[0] - b[0]) + (r[0] - b[0]) * (p[1] - b[1])) / ((g[1] - b[1]) * (r[0] - b[0]) + (b[0] - g[0]) * (r[1] - b[1]));
+    end.b = 1 - end.r - end.g;
+    return end;
+};
+
+var hexToCIE = function(color, gamut) {
+        var r = parseInt(color.substr(0, 2), 16) / 255
+        var g = parseInt(color.substr(2, 2), 16) / 255
+        var b = parseInt(color.substr(4, 2), 16) / 255
+        r = Math.pow(r, (2.2));
+        g = Math.pow(g, (2.2));
+        b = Math.pow(b, (2.2));
+        return toCart(r, g, b, gamut[0], gamut[1], gamut[2])
+    }
+    //compatibilitycurry
+var jankyColor = function(a) {
+    return hexToCIE(a, hueGamut)
+};
+
+var CIEToHex = function(pt, gamut, error) {
+    var error = (error === true) ? "ff0000" : error;
+    var pt = (error === undefined) ? k_co.clipto(pt, gamut[0], gamut[1], gamut[2]) : pt;
+    var bary = toBary(pt, gamut[0], gamut[1], gamut[2]);
+    var min = Math.min(bary.r, bary.g, bary.b);
+    //normalize
+    bary.r = Math.pow(bary.r, (1 / 2.2));
+    bary.g = Math.pow(bary.g, (1 / 2.2));
+    bary.b = Math.pow(bary.b, (1 / 2.2));
+
+    if (min < 0) {
+        return error;
+    } else {
+        return k_co.toHEX(bary.r, bary.g, bary.b)
+    };
+}
+
+//make a function that takes some arguments and outputs a curried put from another thing and allows it to take more arguments
+var fade = function(put, scale, time, steps) {
+    var t = [];
+    for (var i = 0; i < steps; i++) {
+        var pos = (i / (steps - 1))
+        var color = scale(pos).hex().substr(1);
+        var xyc = hexToCIE(color, hueGamut);
+        t[i] = xyc;
+        if (i === 0) {
+            put({
+                xy: t[0],
+                transitime: 0
+            });
+        } else {
+            setTimeout(
+                function(n, tt) {
+                    put({
+                        xy: t[n],
+                        transitiontime: Math.round(tt / 100)
+                    });
+                }, (time * (pos - (1 / steps))), i, (time / steps));
+        };
+    };
+};
+
+//this entire function is awful and needs to go
+var initiate = function(n) {
+    req.open('get', basestation + "api/" + developerid + "/lights", false);
+    req.send();
+    var lindex = JSON.parse(req.response);
+    var lights = [];
+    for (var attrname in lindex) {
+        lights.push(attrname)
+    };
+    lights = lights.map(function(n) {
+        return new Lumi(basestation + "api/" + developerid + "/lights/" + n)
+    });
+    n.lights = lights;
+    n.group0 = new Group(group0uri);
+};
+
+initiate(window);
+
+group0.put({
+    alert: 'select'
+});
+var bez = chroma.interpolate.bezier(['red', 'yellow', 'blue', 'purple']);
+var testscale = chroma.scale(bez).correctLightness(true);
+var rotate = chroma.scale(["red", "blue", "green", "red"]);
+//fade(lights[0].put, testscale, 10000, 5)
---
+++ hueweb.js
@@ -0,0 +1,137 @@
+//points 0,0 and 0.5,0.5 of the cie diagram in image coords
+var zero = [66, 592]
+var ptfive = [376, 280]
+
+var select = lights[0].put;
+
+context = document.getElementById('canvas').getContext('2d');
+var imageObj = new Image();
+//imageObj.onload = function() {
+//  context.drawImage(imageObj, 0, 0);
+//};
+imageObj.src = 'cie1931.jpg';
+
+function drawGamut(gamut) {
+    var cur = function(a) {
+        return transCoord(a, zero, ptfive);
+    }
+    var gamut = gamut.map(cur);
+    context.strokeStyle = '#1a0e1c';
+    context.lineWidth = 0.9;
+    context.beginPath();
+    context.moveTo(gamut[0][0], gamut[0][1]);
+    context.lineTo(gamut[1][0], gamut[1][1]);
+    context.lineTo(gamut[2][0], gamut[2][1]);
+    context.lineTo(gamut[0][0], gamut[0][1]);
+    context.stroke();
+}
+drawGamut(hueGamut);
+
+function drawPoint(a) {
+    var x = a[0]
+    var y = a[1]
+    context.strokeStyle = 'rgba(0,0,0,0.5)';
+    context.beginPath();
+    context.arc(x, y, 3, 0, 2 * Math.PI, true);
+    context.lineWidth = 3;
+    context.stroke();
+    context.strokeStyle = '#edc';
+    context.lineWidth = 1;
+    context.beginPath();
+    context.arc(x, y, 5, 0, 2 * Math.PI, true);
+    context.stroke();
+};
+
+function transCoord(pt, r1, r2) {
+    var oneone = [(r2[0] - r1[0]) * 2, -(r2[1] - r1[1]) * 2];
+    var ptr = [pt[0] * oneone[0], pt[1] * oneone[1]];
+    var back = [ptr[0] + r1[0], (-(ptr[1])) + r1[1]]
+    return back;
+}
+
+function transCoord2(pt, r1, r2) {
+    var oneone = [(r2[0] - r1[0]) * 2, -(r2[1] - r1[1]) * 2];
+    var ptr = [pt[0] - r1[0], pt[1] - r1[1]]
+    var back = [ptr[0] / oneone[0], (-ptr[1]) / oneone[1]];
+    return back;
+}
+var lastclick = transCoord(lights[0].get().state.xy, zero, ptfive)
+
+function mouseClick(e) {
+    var mouseX, mouseY;
+    if (e.offsetX) {
+        mouseX = e.offsetX;
+        mouseY = e.offsetY;
+    } else if (e.layerX) {
+        mouseX = e.layerX;
+        mouseY = e.layerY;
+    }
+    context.clearRect(0, 0, 603, 660);
+    context.drawImage(imageObj, 0, 0)
+    drawGamut(hueGamut);
+    drawPoint([mouseX, mouseY]);
+    lastclick = [mouseX, mouseY];
+    var c = context.getImageData(lastclick[0], lastclick[1], 1, 1).data
+    $("#mainheading").css("color", "#" + k_co.toHEX.apply(null, c))
+        //select({xy:transCoord2([mouseX, mouseY], zero, ptfive), bri:parseInt(document.getElementById('brightSlider').value)});
+}
+var syncslide = function() {
+    $('#brightSlider').val($("#numpho").html());
+}
+var syncslide2 = function() {
+    $("#numpho").html($('#brightSlider').val())
+};
+document.getElementById('numpho').addEventListener("input", syncslide);
+document.getElementById('brightSlider').addEventListener("input", syncslide2);
+$("#one").on('click', function() {
+    spd = 100;
+    var lastclick = transCoord(lights[0].get().state.xy, zero, ptfive);
+    select = lights[0].put
+});
+$("#two").on('click', function() {
+    spd = 100;
+    select = lights[1].put
+});
+$("#three").on('click', function() {
+    spd = 100;
+    select = lights[2].put
+});
+$("#all").on('click', function() {
+    spd = 1000;
+    select = group0.put
+});
+$("#on").on('click', function() {
+    select({
+        on: true
+    })
+});
+$("#off").on('click', function() {
+    select({
+        on: false
+    })
+});
+document.getElementById('canvas').onmousedown = function(e) {
+    mouseClick(e);
+    document.getElementById('canvas').onmousemove = mouseClick;
+};
+document.getElementById('canvas').onmouseup = function() {
+    document.getElementById('canvas').onmousemove = undefined;
+};
+$(window).blur(function() {
+    clearInterval(test);
+});
+$(window).focus(function() {
+    test = setInterval(sendcomm, spd)
+});
+var spd = 1000;
+var sendcomm = function() {
+    select({
+        xy: transCoord2(lastclick, zero, ptfive),
+        bri: parseInt(document.getElementById('brightSlider').value),
+        transitiontime: (spd / 100)
+    })
+    clearInterval(test);
+    test = setInterval(sendcomm, spd)
+}
+test = setInterval(sendcomm, spd)
+    //clearInterval(test);
